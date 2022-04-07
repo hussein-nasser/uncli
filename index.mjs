a66bdc40a0a3c9420bc3a2c7aa739100a5cc9712
Hussein Nasser <huss8228@esri.com>
2022-04-07 15:11:26 -0700

    71:

--- index.mjs
+++ index.mjs
@@ -7,7 +7,7 @@ import { AdminLog } from "./adminlog.mjs"
 import  logger  from "./logger.mjs"
 import  fetch  from "node-fetch"
 //update version
-let version = "0.0.70";
+let version = "0.0.71";
 const GENERATE_TOKEN_TIME_MIN = 30;
 
 let rl = null;
